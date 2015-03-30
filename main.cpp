5b082690db5d747bf854355fa10745d1ab8fdde0
matrix1367 <matrix1367@o2.pl>
2015-03-30 17:04:59 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: mbettin
+ *
+ * Created on 30 marca 2015, 17:00
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
